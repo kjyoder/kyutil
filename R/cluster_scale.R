a4071d01dec23398f81e5e02fc50d3900c93c7de
kjyoder <kjyoder@gmail.com>
2022-01-31 11:48:48 -0600

    add cluster scaling and use proper enquo()

--- R/cluster_scale.R
+++ R/cluster_scale.R
@@ -1,4 +1,4 @@
-#' @title Perform cluster mean centering
+#' @title Perform cluster mean centering and convert to cluster-specific z-scores
 #'
 #' @description For the given data frame,
 #' calculate the mean of the given column ('x')
@@ -6,50 +6,50 @@
 #'
 #' A new data.frame is returned with two additional columns,
 #'   *_m the mean of x within each cluster
-#'   *_s the individual values of x, centered at 0
+#'   *_z the z-scored values of x
 #'
 #' @param df data.frame containing the variables
 #' @param x char; label of the column to cluster mean center
 #' @param g char; label of the column with cluster ids
 #' @param label char; to use for new columns (default=x)
-#' @param clobber logical; to control behavior when a column <label>_m or <label>_s already exists. TRUE: overwrite existing column. FALSE: exit function (default=FALSE)
+#' @param clobber logical; to control behavior when a column <label>_m or <label>_z already exists. TRUE: overwrite existing column. FALSE: exit function (default=FALSE)
 #'
 #' @return data.frame,tibble
 #' @docType package
-#' @name cluster_mean
+#' @name cluster_scale
 #' @examples
 #' df <- tibble(x=rnorm(10), g=rep(c(0,1),5))
-#' df <- cluster_mean(df, 'x', 'g')
+#' df <- cluster_scale(df, x, g)
 #' df
 #' # A tibble: 10 x 4
-#'           x     g    x_m     x_s
-#'       <dbl> <dbl>  <dbl>   <dbl>
-#'  1 -0.918       0 -0.565 -0.353
-#'  2  0.355       1  0.280  0.0753
-#'  3  0.0313      0 -0.565  0.596
-#'  4  0.858       1  0.280  0.578
-#'  5 -0.201       0 -0.565  0.364
-#'  6  1.88        1  0.280  1.60
-#'  7 -0.907       0 -0.565 -0.342
-#'  8 -1.69        1  0.280 -1.97
-#'  9 -0.830       0 -0.565 -0.265
-#' 10 -0.00634     1  0.280 -0.286
+#'       x     g   x_m    x_z
+#'   <dbl> <dbl> <dbl>  <dbl>
+#' 1 -0.680     0 0.260 -1.10
+#' 2  0.927     1 0.350  0.484
+#' 3 -0.590     0 0.260 -0.993
+#' 4  1.44      1 0.350  0.912
+#' 5  1.10      0 0.260  0.984
+#' 6 -0.625     1 0.350 -0.817
+#' 7  0.447     0 0.260  0.218
+#' 8 -1.22      1 0.350 -1.31
+#' 9  1.02      0 0.260  0.889
+#'10  1.23      1 0.350  0.735
 #'
 #' @export
-cluster_mean <- function(df, x, g, label, clobber=FALSE) {
+cluster_scale <- function(df, x, g, label, clobber=FALSE) {
   require(dplyr)
   require(stringr)
   require(rlang)
-
+  # set up new column names
   x <- enquo(x)
   g <- enquo(g)
-  # set up new column names
-  if (is_missing(label))
+  if (is_missing(label)) {
     label <- get_expr(x)
+  }
   m_col = str_c(label, '_m')
-  s_col = str_c(label, "_s")
-  new_cols = c('mreservedkey', 'sreservedkey')
-  names(new_cols) <- c(m_col, s_col)
+  z_col = str_c(label, "_z")
+  new_cols = c('mreservedkey', 'zreservedkey')
+  names(new_cols) <- c(m_col, z_col)
 
   # check for pre-existing columns to prevent accidental clobbering
   for (col in names(new_cols)) {
@@ -66,11 +66,13 @@ cluster_mean <- function(df, x, g, label, clobber=FALSE) {
   dm <- df %>%
     dplyr::select(!!g, !!x) %>%
     group_by(!!g) %>%
-    summarize(mreservedkey = mean(!!x, na.rm=TRUE))
+    summarize(mreservedkey = mean(!!x, na.rm=TRUE),
+              reservedsd = sd(!!x, na.rm=TRUE))
   # add means to df, calculate cluster-centered values, then rename new columns
   d <- df %>%
     left_join(dm, by=as_name(g)) %>%
-    mutate(sreservedkey = !!x - mreservedkey) %>%
+    mutate(zreservedkey = (!!x - mreservedkey)/reservedsd) %>%
+    select(-reservedsd) %>%
     rename(!!new_cols)
 
   return(d)
